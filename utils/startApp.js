9fc970985b33768043c7edca61077fa1e4352bc1
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2024-09-12 21:04:26 -0400

    created all until needed.

---
+++ utils/startApp.js
@@ -0,0 +1,8 @@
+// TODO: import all and any functions/files here :)
+import logoutButton from '../components/logoutButton';
+
+const startApp = () => {
+  logoutButton();
+  // TODO: put all things that need to go on the DOM on appload
+};
+export default startApp;
