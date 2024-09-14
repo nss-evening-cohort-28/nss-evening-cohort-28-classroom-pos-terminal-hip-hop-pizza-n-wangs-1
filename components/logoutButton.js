86ac795b7250e9a5fcee124aeb6dc1c5d598b038
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2024-09-14 14:51:17 -0400

    multiuser auth login

--- components/logoutButton.js
+++ components/logoutButton.js
@@ -1,7 +1,14 @@
 import { signOut } from '../utils/auth';
+import clearDom from '../utils/clearDom';
 
 const logoutButton = () => {
-  document.querySelector('#logout-button').addEventListener('click', signOut);
+  const domString = '<button id="google-auth" class="btn btn-danger">SIGNOUT</button>';
+  document.querySelector('#logout-button').innerHTML = (domString);
+  document.querySelector('#logout-button').addEventListener('click', () => {
+    signOut(); // Your sign-out function
+    clearDom(); // Another function to clear DOM elements
+    window.location.reload(); // Reload the page
+  });
 };
 
 export default logoutButton;
