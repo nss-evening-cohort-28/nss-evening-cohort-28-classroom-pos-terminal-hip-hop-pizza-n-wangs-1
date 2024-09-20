916d6a9286ca37a7eab32bf1769b156669bf8f9e
Tion Blackmon <tion.blackmon@outlook.com>
2024-09-19 19:29:29 -0500

    Using Bootstrap to center elements and make them more responsive.

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -3,10 +3,10 @@ import renderToDom from '../../utils/renderToDom';
 const domBuilder = () => {
   const domString = `
   <div id="navigation"></div>
-  <div id="main-container">
+  <div id="main-container" class="container">
     <div id="add-button"></div>
     <div id="form-container"></div>
-    <div id="store"></div>
+    <div id="store" class="container text-center row row-cols-4"></div>
     <div id="view"></div>
   </div>`;
 
